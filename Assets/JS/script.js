fdf5901c714446f8393a88d94fa7ef2bc1b1ce57
Jeffrey Young <jeffreymiyoung@aol.com>
2022-04-13 23:26:47 -0400

    added JS Variables and APIKey for OpenWeather

---
+++ Assets/JS/script.js
@@ -0,0 +1,19 @@
+// variable declaration
+var city = "";
+var searchCity = $("#city-input");
+var searchButton = $("#search-btn");
+var clearButton = $("#clear-btn");
+var selectedCity = $("#selected-city");
+var currentTemp = $("#temperature");
+var currentWind = $("#wind");
+var currentHumid = $("#humidity");
+var currentUv = $("#uv");
+
+// API Key
+var APIKey = "b0febebb4847e59a642add06c013d2fe";
+
+// Search History Array Declaration
+var cityHistory = [];
+
+
+
